b4c6d667f283610f7bb583ae95b9eef2f7c6c2b0
Chileshe <64306558+Abraham-Chileshe@users.noreply.github.com>
2025-11-25 03:14:43 +0200

    Full code, but need rework on the function repetition

--- Cryptography/Caeser_Cipher.cpp
+++ Cryptography/Caeser_Cipher.cpp
@@ -1,17 +1,73 @@
 #include <iostream>
 #include <string>
 
+#define line std::cout<<"----------------------------------"<<std::endl;
+
 char shiftCipher(char c, int shift) {
+	char new_c;
+
 	if (c >= 'A' && c <= 'Z') {
-		char new_c = (c - 'A' + shift) % 26 + 'A';
+		new_c = (c - 'A' + shift) % 26 + 'A';
 		return new_c;
 	}
-	else
+	else if (c >= 'a' && c <= 'z') {
+		new_c = (c - 'a' + shift) % 26 + 'a';
+	}
+	else {
 		return c;
+	}
 }
 
 int main() {
-	std::cout << shiftCipher('A', 2);
+	std::string text;
+	std::string result = "";
+	int shift;
+	int choice;
+
+	std::cout << "Caeser Encryption Algorithm" << std::endl;
+	line;
+	std::cout << "1. Encrypt Message" << std::endl;
+	std::cout << "2. Decrypt Message" << std::endl;
+	line;
+	std::cout << "enter choice >> ";
+	std::cin >> choice;
+	line;
+	if (choice == 1 || choice == 2) {
+		std::cout << "Enter Text: ";
+		std::cin >> text;
+
+		std::cout << "Enter shift >> ";
+		std::cin >> shift; 
+
+		if (choice == 1) {
+			// Process each character
+			for (int i = 0; i < text.length(); i++) {
+				result += shiftCipher(text[i], shift);
+			}
+
+			std::cout << "CipherText:";
+			std::cout << result<< std::endl;
+		}
+		else if (choice == 2) {
+			shift = -shift;
+			// Process each character
+			for (int i = 0; i < text.length(); i++) {
+				result += shiftCipher(text[i], shift);
+			}
+
+			std::cout << "CipherText:";
+			std::cout << result << std::endl;
+		}
+
+		line;
+	}
+	else {
+		std::cout << "invalid input";
+		line;
+	}
+
+
+
 
 	return 0;
 
